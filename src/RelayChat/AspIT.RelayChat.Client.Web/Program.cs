99f85c7a2552679766f4032877e98d2106f283f1
Tobsi <61752@edu.campusvejle.dk>
2024-12-05 04:18:52 -0600

    Cleanup and moved the web client to a standalone project

---
+++ src/RelayChat/AspIT.RelayChat.Client.Web/Program.cs
@@ -0,0 +1,25 @@
+using AspIT.RelayChat.SignalRLibrary;
+using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using System;
+
+namespace AspIT.RelayChat.Client.Web
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+            builder.RootComponents.Add<HeadOutlet>("head::after");
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddSingleton<Chat>();
+
+            builder.Services.AddScoped<UsernameState>(); // This is a state container for the username
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
